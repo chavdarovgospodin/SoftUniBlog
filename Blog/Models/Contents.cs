291ac98133e33c418fe0daed70b15c93ae59c29f
gospodin <gospodin.chavdarov92@gmail.com>
2016-12-22 17:14:49 +0200

    Last commit

---
+++ Blog/Models/Contents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Contents
+    {
+
+        [ForeignKey("Contents")]
+        public int CategoryId { get; set; }
+
+        public virtual Contents Content { get; set; }
+
+
+    }
+}
